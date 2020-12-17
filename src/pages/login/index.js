848d88d8b6127961c18d7a856d0ae534f09ff226
Rebeca Canesin <rebecafguilherme@gmail.com>
2020-12-17 19:36:52 -0300

    cadastrando e logando usuarios

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -13,57 +13,55 @@ export const Login = () => {
         <input type="password" id="password" placeholder="Senha"></input>
         <button type="submit" id="lgnBtn">Login</button>
         <button type="submit" id="sgnUpBtn">Cadastrar</button>
+        <button type="submit" id="sgnOutBtn">Logout</button>
 
       </form>
   `;
 
   const emailTxt = rootElement.querySelector("#email");
   const  passTxt = rootElement.querySelector("#password");
-  // let email  = emailTxt.value;
-  // let  password = passTxt.value;
+
   const loginButton = rootElement.querySelector("#lgnBtn");
   loginButton.addEventListener("click", (event) =>{
-  //   firebase.auth().signInWithEmailAndPassword(email, password)
-  //   .then(()=> console.log("eita"))
-  // });
-    
     console.log(passTxt.value);
     event.preventDefault();
     
-    firebase.auth().createUserWithEmailAndPassword(emailTxt.value, passTxt.value)
+    firebase.auth().signInWithEmailAndPassword(emailTxt.value, passTxt.value)
     .then((user) => {
       console.log("eita");
-      // Signed in
-      // ...
     })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
-      // ..
     });
   });
 
 
-
-
-
-
-  // const signUpButton = rootElement.querySelector("#sgnUpBtn");
-  // signUpButton.addEventListener("click", (e)=>{
-  //   e.preventDefault();
-  //   firebase.auth().createUserWithEmailAndPassword(email, password);
-  // });
+  const signUpButton = rootElement.querySelector("#sgnUpBtn");
+  signUpButton.addEventListener("click", (e)=>{
+    console.log(emailTxt.value);
+    e.preventDefault();
+    firebase.auth().createUserWithEmailAndPassword(emailTxt.value, passTxt.value)
+    .then((user) => {
+      console.log("eita");
+    })
+    .catch((error) => {
+      
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  });
 
   
 
-  //   const stateChange =  firebase.auth().onAuthStateChanged(firebaseUser => {
-  //       if(firebaseUser){
-  //         console.log(firebaseUser);
-  //       }else{
-  //         console.log("not logged in");
-  //       }
+    // const stateChange =  firebase.auth().onAuthStateChanged(firebaseUser => {
+    //     if(firebaseUser){
+    //       console.log(firebaseUser);
+    //     }else{
+    //       console.log("not logged in");
+    //     }
     
-  //   });
+    // });
     
 
   
--- src/services/index.js
+++ src/services/index.js
@@ -5,35 +5,3 @@ export const myFunction = () => {
   console.log('Olá mundo!');
 };
 
-// const firebaseConfig = {
-//   apiKey: "API_KEY",
-//   authDomain: "PROJECT_ID.firebaseapp.com",
-//   databaseURL: "https://PROJECT_ID.firebaseio.com",
-//   projectId: "PROJECT_ID",
-//   storageBucket: "PROJECT_ID.appspot.com",
-//   messagingSenderId: "SENDER_ID",
-//   appId: "APP_ID",
-// };
-
-// firebase.initializeApp(firebaseConfig);
-
-
-// export const loginUser = () => firebase.auth().signInWithEmailAndPassword(email, password);
-
-   
-
-// export const signUp = () => firebase.auth().createUserWithEmailAndPassword(email, password);
-  
-
-
-// export const state = () => {
-
-//     firebase.auth().onAuthStateChanged(firebaseUser => {
-//     if(firebaseUser){
-//       console.log(firebaseUser);
-//     }else{
-//       console.log("not logged in");
-//     }
-
-//   });
-// }
